9530a10c91dc7fc434f8e29e85b94542c5cc3cfc
Lester Ivan Nollora <palakol80@gmail.com>
2024-04-24 21:24:32 +1000

    added funtion

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log('hello world!')
 console.log("Hello partner A!");
-console.log("Hello partner B!")
+console.log("Hello partner B!");
+console.log(`kahit ano`)
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(1,2))
